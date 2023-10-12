0b27e90454556b94bdb7b7e194199442142fb2e0
Abhishek-M-K <khandareabhishek98@gmail.com>
2023-10-12 21:45:51 +0530

    second commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
